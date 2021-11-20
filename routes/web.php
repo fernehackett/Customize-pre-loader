8dc97cedc85f7235cc397e599ce1136caef4fede
Bocbocboc <dinhvanhieu927@gmail.com>
2021-11-20 10:17:54 +0700

    dvh

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['middleware' => ['verify.shopify']], function () {
+Route::group(['middleware' => ['verify.shopify', 'billable']], function () {
     Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
-    Route::post('/uninstall', 'App\Http\Controllers\HomeController@uninstall')->name('uninstall');
+    Route::resource('settings/get/data', 'App\Http\Controllers\SettingController');
     Route::resource('settings', 'App\Http\Controllers\SettingController');
 
 });
-Route::post('customer/data-request', 'App\Http\Controllers\HomeController@customerDataRequest');
-Route::post('customer/redact', 'App\Http\Controllers\HomeController@customerRedact');
-Route::post('shop/redact', 'App\Http\Controllers\HomeController@shopRedact');
+Route::group(['middleware' => ['auth.webhook']], function () {
+    Route::post('/uninstall', 'App\Http\Controllers\HomeController@uninstall')->name('uninstall');
+    Route::post('customer/data-request', 'App\Http\Controllers\HomeController@customerDataRequest');
+    Route::post('customer/redact', 'App\Http\Controllers\HomeController@customerRedact');
+    Route::post('shop/redact', 'App\Http\Controllers\HomeController@shopRedact');
+});
 Route::get('policy', 'App\Http\Controllers\HomeController@policy')->name("policy");
